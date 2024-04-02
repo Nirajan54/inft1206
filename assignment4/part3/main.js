0a95e541ea7290b3f60a687f690d1e4c0d926667
Nirajan54 <156444822+Nirajan54@users.noreply.github.com>
2024-04-01 20:43:28 -0400

    assignment4 part3

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -1,24 +1,22 @@
 // setup canvas
-
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-// Ball Class
+// Creating Ball Class
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -28,6 +26,14 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
   update() {
     if ((this.x + this.size) >= width) {
       this.velX = -(this.velX);
@@ -48,13 +54,6 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
-  
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
 
   collisionDetect() {
     for (const ball of balls) {
@@ -71,6 +70,7 @@ class Ball {
   }
   
 }
+
 const balls = [];
 
 while (balls.length < 25) {
@@ -103,5 +103,3 @@ function loop() {
 }
 
 loop();
-
-
